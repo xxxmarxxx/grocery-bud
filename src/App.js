67865f63db60e321be310851016d3fc3de687514
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-01 16:39:05 +0100

    lesson 130

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
-// lesson 128/129
+// lesson 128/129/130
 
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
+      showAlert(true, "danger", "please enter value");
       // display alert
     } else if (name && isEditing) {
       // deal with edit
@@ -23,10 +28,15 @@ function App() {
     }
     // console.log("hello");
   };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
